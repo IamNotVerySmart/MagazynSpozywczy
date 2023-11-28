da78becc932f676c50dad9d009fe8e15a541b728
IamNotVerySmart <93706080+IamNotVerySmart@users.noreply.github.com>
2023-11-28 15:12:11 +0100

    more menu updates
    more menu updates

---
+++ MagazynSpozywczy/Werehouse/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werehouse
+{
+    public class Product
+    {
+        string ProducerName { get; set; }
+        string ProductName { get; set; }
+        string Category { get; set; }
+        int ProductCode { get; set; }
+        double Price { get; set; }
+        string Description { get; set; }
+
+        public Product(string producerName, string productName, string category, int productCode, double price, string description)
+        {
+            ProducerName = producerName;
+            ProductName = productName;
+            Category = category;
+            ProductCode = productCode;
+            Price = price;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"Producent: {ProducerName},\nProdukt: {ProductName},\nKategoria: {Category}.\nKod produktu: {ProductCode},\nCena: {Price},\nOpis: {Description}\n";
+        }
+    }
+}
---
+++ MagazynSpozywczy/Werehouse/Werehouse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werehouse
+{
+    internal class Magazyn
+    {
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+        public Adress Adress { get; set; }
+
+        public Magazyn(List<Product> products, Adress adress, string name)
+        {
+            Products = products;
+            Adress = adress;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Nazwa: {Name}\n";
+        }
+    }
+}
